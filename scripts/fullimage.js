03dbeabba9232277238ffbfaca125a083d0a7ca8
Emirhan-Oz <pdemirozz@gmail.com>
2024-08-06 17:07:52 +0000

    sp

--- scripts/fullimage.js
+++ scripts/fullimage.js
@@ -6,26 +6,57 @@ const theVideo = document.querySelector('#theVideo');
 const sourceVideo = document.querySelector('#sourceVideo');
 
 imgs.forEach(img => {
-    img.addEventListener('click', function() {
+    img.addEventListener('click', function () {
         fullpage_img.style.display = "block";
         fullpage_img.style.backgroundImage = 'url(' + img.src + ')';
         fullPage.style.display = 'block';
     });
 });
 
-videos.forEach(video => {
-    video.addEventListener('click', function() {
-        theVideo.src = video.currentSrc;
-        fullPage.style.display = 'block';
-        theVideo.style.display = "block";
-        theVideo.play();
-        theVideo.muted = true;
-    });
-});
-
-fullPage.addEventListener("click", ()=>{
+fullPage.addEventListener("click", () => {
     theVideo.style.display = 'none';
     fullPage.style.display = "none";
     fullpage_img.style.display = "none";
     theVideo.stop();
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+    const videos = document.querySelectorAll('.videos');
+
+    videos.forEach(video => {
+        // Videoyu başlangıçta duraklat
+        video.pause();
+
+        // Mouse ile üzerine gelindiğinde video oynat
+        video.addEventListener('mouseover', () => {
+            video.play();
+            video.classList.add('playing');
+        });
+
+        // Mouse ile videodan çıkıldığında video duraklat
+        video.addEventListener('mouseout', () => {
+            video.pause();
+            video.classList.remove('playing');
+        });
+
+        // Mobil cihazlarda basılı tutulduğunda video oynat
+        video.addEventListener('touchstart', () => {
+            video.play();
+            video.classList.add('playing');
+        });
+
+        // Mobil cihazlarda basılı tutma sonrasında video duraklat
+        video.addEventListener('touchend', () => {
+            video.pause();
+            video.classList.remove('playing');
+        });
+
+        video.addEventListener('click', function () {
+            theVideo.src = video.currentSrc;
+            fullPage.style.display = 'block';
+            theVideo.style.display = "block";
+            theVideo.play();
+            theVideo.muted = true;
+        });
+    });
+});
